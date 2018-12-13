fcc90fca7a44ee307dbcc7d7e77d0ec8028740ea
pallavid-cuelogic <pallavi.deshmukh@cuelogic.com>
2018-12-13 17:30:45 +0530

    modifications in Assignment

--- Router/LastLoggedIn.js
+++ Router/LastLoggedIn.js
@@ -5,18 +5,7 @@ var userActivity = require('../Model/UserActivity');
 var authenticate = require('../Middleware/Validation');
 
 router.get("/", (req, res, next) => {
-    // var now = new Date();
-    // userActivity.find({},(err,result) => {
-    //     console.log(result);
-    //     var len = Object.keys(result).length
-    //     for(var i = 0;i<len;i++){
-    //         var loginDates = result[i].loginDate;
-    //         var date1=moment(loginDates);
-    //         //console.log("date in moment format" + date1);
-    //         //console.log("logindates",loginDates)
-    //         var diff = loginDates - now;
-    //         console.log(diff)
-    //         }
+    res.sendFile(path.join('/home/pallavi/CuelogicWork/Node/Final_Assignment' + '/Public'+'/LastLoggedIn.html'))   
 })
 
 module.exports = router;
